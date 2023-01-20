8ad4365e65a4156414d8d13391ea8c9503c9cb6e
Michael <Michael.evans1775@outlook.com>
2023-01-20 13:31:45 -0700

    Created Contacts Class
    added setters/getters

--- src/main/java/com/michael/c195_software2/Contacts.java
+++ src/main/java/com/michael/c195_software2/Contacts.java
@@ -1,4 +1,42 @@
 package com.michael.c195_software2;
 
 public class Contacts {
+    private int contactID;
+    private String contactName;
+    private String email;
+
+    public Contacts(int contactID, String contactName, String email){
+        this.contactID = contactID;
+        this.contactName = contactName;
+        this.email = email;
+    }
+
+    public Contacts(){
+
+    }
+
+
+    public void setContactID(int contactID) {
+        this.contactID = contactID;
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
