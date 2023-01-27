d9f00c063daf052ab1ab1ce3e93f71f5a73d582a
Michael <Michael.evans1775@outlook.com>
2023-01-27 10:15:53 -0700

    Customerview WIP

---
+++ src/main/java/com/michael/c195_software2/DataAccessObject/CustomerDAO.java
@@ -0,0 +1,4 @@
+package com.michael.c195_software2.DataAccessObject;
+
+public class CustomerDAO {
+}
--- src/main/java/com/michael/c195_software2/customer_view_controller.java
+++ src/main/java/com/michael/c195_software2/customer_view_controller.java
@@ -1,4 +1,35 @@
 package com.michael.c195_software2;
 
-public class customer_view_controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class customer_view_controller implements Initializable {
+    @FXML
+    private TableView<Customers> customerTable;
+    @FXML
+    private TableColumn<?,?> idCOL;
+    @FXML
+    private TableColumn<?,?> nameCOL;
+    @FXML
+    private TableColumn<?,?> addressCOL;
+    @FXML
+    private TableColumn<?,?> postalCOL;
+    @FXML
+    private TableColumn<?,?> phoneCOL;
+
+
+    /**
+     * This method is used to populate the tableview found on customer view.
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
