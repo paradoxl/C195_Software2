ace8efda6ac7915c0a02c4c3cb9422be994b8120
Michael <Michael.evans1775@outlook.com>
2023-01-27 10:11:07 -0700

    added javadoc

--- src/main/java/com/michael/c195_software2/Customers.java
+++ src/main/java/com/michael/c195_software2/Customers.java
@@ -1,7 +1,6 @@
 package com.michael.c195_software2;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+
 
 public class Customers {
     private int customerID;
@@ -11,6 +10,14 @@ public class Customers {
     private int divisionID;
 
 
+    /**
+     * Custom constructor
+     * @param customerID
+     * @param customerName
+     * @param address
+     * @param postalCode
+     * @param divisionID
+     */
     public Customers(int customerID, String customerName, String address, String postalCode,int divisionID ){
         this.customerID = customerID;
         this.customerName = customerName;
@@ -18,46 +25,90 @@ public class Customers {
         this.postalCode = postalCode;
         this.divisionID = divisionID;
     }
+
+    /**
+     * custom empty constructor
+     */
     public Customers(){
 
     }
 
+    /**
+     * set customer id
+     * @param customerID
+     */
     public void setCustomerID(int customerID) {
         this.customerID = customerID;
     }
 
+    /**
+     * get customer id
+     * @return
+     */
     public int getCustomerID() {
         return customerID;
     }
 
+    /**
+     * set customer name
+     * @param customerName
+     */
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
+    /**
+     * get customer name
+     * @return
+     */
     public String getCustomerName() {
         return customerName;
     }
 
+    /**
+     * set address
+     * @param address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * get address
+     * @return
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * set postal code
+     * @param postalCode
+     */
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
 
+    /**
+     * get postal code
+     * @return
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * set division id
+     * @param divisionID
+     */
     public void setDivisionID(int divisionID) {
         this.divisionID = divisionID;
     }
 
+    /**
+     * get division id
+     * @return
+     */
     public int getDivisionID() {
         return divisionID;
     }
