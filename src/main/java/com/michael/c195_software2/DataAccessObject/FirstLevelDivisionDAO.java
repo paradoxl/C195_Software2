44168702e13b99dbcbbac6fd6b197227eb874983
Michael <Michael.evans1775@outlook.com>
2023-01-30 09:09:40 -0700

    FLD DAO finished

--- src/main/java/com/michael/c195_software2/DataAccessObject/FirstLevelDivisionDAO.java
+++ src/main/java/com/michael/c195_software2/DataAccessObject/FirstLevelDivisionDAO.java
@@ -1,5 +1,38 @@
 package com.michael.c195_software2.DataAccessObject;
 
+import com.michael.c195_software2.FirstLevelDivisions;
+import com.michael.c195_software2.con.InitCon;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 public class FirstLevelDivisionDAO {
 
+
+    public  static ObservableList<FirstLevelDivisions> getFLD() throws SQLException {
+//        FirstLevelDivisions FLD = new FirstLevelDivisions();
+        ObservableList<FirstLevelDivisions> FLD = FXCollections.observableArrayList();
+        String query = "SELECT * FROM first_level_divisions";
+        PreparedStatement ps = InitCon.connection.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+        System.out.println("FLD Query Executed");
+        while (rs.next()) {
+            int DivID = rs.getInt("Division_ID");
+            String div = rs.getString("Division");
+            LocalDateTime created = rs.getTimestamp("Create_Date").toLocalDateTime();
+            String createdBy = rs.getString("Created_By");
+            LocalDateTime lastUpdate = rs.getTimestamp("Last_Update").toLocalDateTime();
+            String lastUpdateBy = rs.getString("Last_Updated_By");
+            int countryID = rs.getInt("Country_ID");
+
+            FirstLevelDivisions firstLevel = new FirstLevelDivisions(DivID,div,created,createdBy,lastUpdate,lastUpdateBy,countryID);
+            FLD.add(firstLevel);
+        }
+
+        return FLD;
+    }
 }
