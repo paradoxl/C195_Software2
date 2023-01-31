d25d04d764556265bef7648f0ee5be86e117315c
Michael <Michael.evans1775@outlook.com>
2023-01-31 12:48:03 -0700

    cancel button imp

--- src/main/java/com/michael/c195_software2/update_customer_view_controller.java
+++ src/main/java/com/michael/c195_software2/update_customer_view_controller.java
@@ -1,9 +1,22 @@
 package com.michael.c195_software2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class update_customer_view_controller {
+    @FXML
+    public Button save;
+    @FXML
+    public Button cancel;
     @FXML
     private TextField nameTextFLD;
     @FXML
@@ -13,6 +26,7 @@ public class update_customer_view_controller {
     @FXML
     private TextField phoneTextFLD;
 
+    Alert cancelAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you wish to cancel?", ButtonType.YES, ButtonType.NO);
 
     private Customers selected;
     public void populate(Customers selected){
@@ -31,5 +45,23 @@ public class update_customer_view_controller {
         phoneTextFLD.setText(phone);
 
 
+    }
+
+    public void cancel(ActionEvent actionEvent) throws IOException {
+        cancelAlert.showAndWait();
+        if(cancelAlert.getResult() == ButtonType.YES){
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("customer-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("Customer Records");
+            stage.setScene(scene);
+            stage.show();
+        }
+        else{
+            System.out.println("Why push it then?");
+        }
+    }
+
+    public void save(ActionEvent actionEvent) {
     }
 }
