fecb073a5707d0bb01d24a8b6373e58dab9f7295
Michael <Michael.evans1775@outlook.com>
2023-02-13 10:02:56 -0700

    fixed combo box on add.
    wip update

--- src/main/java/com/michael/c195_software2/update_customer_view_controller.java
+++ src/main/java/com/michael/c195_software2/update_customer_view_controller.java
@@ -8,19 +8,22 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ResourceBundle;
 
 /**
  * This class is used to update the customer records.
  */
-public class update_customer_view_controller {
+public class update_customer_view_controller implements Initializable {
     @FXML
     public Button save;
     @FXML
@@ -72,7 +75,6 @@ public class update_customer_view_controller {
         phoneTextFLD.setText(phone);
 
 
-
         //we need to pull in the current selected values and then populate.
         hiddenVar.setOpacity(0);
         try {
@@ -95,17 +97,17 @@ public class update_customer_view_controller {
 
                 for(int i = 0; i < 51; i++){
                     if(selected.getDivisionID() == i) {
-                        CBOX.setValue("U.S.");
+                        CBOX.setValue("U.S");
                     }
                 }
                 for(int i = 64; i < 104; i++){
                     if(selected.getDivisionID() == i) {
-                        CBOX.setValue("U.K.");
+                        CBOX.setValue("UK");
                     }
                 }
                 for(int i = 51; i < 64; i++){
                     if(selected.getDivisionID() == i) {
-                        CBOX.setValue("CANADA");
+                        CBOX.setValue("Canada");
                     }
                 }
 
@@ -114,7 +116,7 @@ public class update_customer_view_controller {
             CBOX.setItems(countryVal);
 
             // Hides the state/prov box until a country selection is made.
-            SBOX.setOpacity(0);
+//            SBOX.setOpacity(0);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -190,7 +192,7 @@ public class update_customer_view_controller {
 
         saveAlert.showAndWait();
         if(saveAlert.getResult() == ButtonType.YES) {
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("customer-view.fxml"));
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("customer-view.fxmimplements Initializablel"));
             Scene scene = new Scene(loader.load());
             Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
             stage.setTitle("Customer Records");
@@ -249,4 +251,47 @@ public class update_customer_view_controller {
         // query db based off division
 
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        hiddenVar.setOpacity(0);
+        try {
+            ObservableList<FirstLevelDivisions> FLD = FirstLevelDivisionDAO.getFLD();
+            ObservableList<String> fldVAL = FXCollections.observableArrayList();
+            ObservableList<Integer> fldCC = FXCollections.observableArrayList();
+            ObservableList<String> finalFLDVALUS = FXCollections.observableArrayList();
+            ObservableList<String> finalFLDVALCA = FXCollections.observableArrayList();
+            ObservableList<String> finalFLDVALUK = FXCollections.observableArrayList();
+
+            CBOX.setOnAction( e ->{
+                FLD.forEach((firstLevelDivisions -> fldCC.add(firstLevelDivisions.getCountryID())));
+                FLD.forEach(firstLevelDivision -> fldVAL.add(firstLevelDivision.getDivision()));
+                if(CBOX.getSelectionModel().getSelectedIndex() == 0){
+                    SBOX.setOpacity(100);
+                    for(int i = 0; i < 51; i++){
+                        finalFLDVALUS.add(fldVAL.get(i));
+                    }
+                    SBOX.setItems(finalFLDVALUS);
+                }
+                if(CBOX.getSelectionModel().getSelectedIndex() == 1){
+                    SBOX.setOpacity(100);
+                    for(int i = 64; i < 68; i++){
+                        finalFLDVALUK.add(fldVAL.get(i));
+                    }
+                    SBOX.setItems(finalFLDVALUK);
+                }
+                if(CBOX.getSelectionModel().getSelectedIndex() == 2){
+                    SBOX.setOpacity(100);
+                    for(int i = 51; i < 64; i++){
+                        finalFLDVALCA.add(fldVAL.get(i));
+                    }
+                    SBOX.setItems(finalFLDVALCA);
+                }
+
+            });
+    }catch (SQLException e){
+            Alert cancelAlert = new Alert(Alert.AlertType.ERROR, e.toString(), ButtonType.OK);
+            cancelAlert.showAndWait();
+        }
+        }
 }
