4b013f40295831c1dbdea66887be078d3546d275
Michael Evans <Michael.evans1775@outlook.com>
2023-02-14 10:38:57 -0700

    wip

--- src/main/java/com/michael/c195_software2/DataAccessObject/AppointmentDAO.java
+++ src/main/java/com/michael/c195_software2/DataAccessObject/AppointmentDAO.java
@@ -35,12 +35,6 @@ public class AppointmentDAO {
         ResultSet rs = ps.executeQuery();
         System.out.println("Gathering appointments");
 
-        //UTC conversion
-        LocalDateTime now = LocalDateTime.now();
-        ZonedDateTime conv = now.atZone(ZoneId.systemDefault());
-        ZonedDateTime utc = conv.withZoneSameInstant(ZoneOffset.UTC);
-
-        int difference = utc.getHour() - now.getHour();
 
         while (rs.next()){
             int appointmentId = rs.getInt("Appointment_ID");
@@ -48,7 +42,6 @@ public class AppointmentDAO {
             String title = rs.getString("Title");
             String location =  rs.getString("Location");
             String type = rs.getString("Type");
-//            LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime().minusHours(difference);
             LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
             LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
             LocalDateTime Created = rs.getTimestamp("Create_Date").toLocalDateTime();
@@ -56,23 +49,27 @@ public class AppointmentDAO {
             int userID = rs.getInt("User_ID");
             int contactID = rs.getInt("Contact_ID");
 
+            //Formatters if needed for debugging.
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
             start.format(formatter);
             DateFormat timeOnly  = new SimpleDateFormat("HH:mm:ss");
             DateFormat dateOnly  = new SimpleDateFormat("MM:dd:yyyy");
 
-            //Converting times to UTC then to LDT
+            //Converting start times to UTC then to LDT
             ZonedDateTime startConv = start.atZone(ZoneId.of("UTC")); // set LDT to ZDT with utc timezone
             ZonedDateTime startLDT = startConv.withZoneSameInstant(ZoneId.systemDefault()); // convert UTC to local
             LocalDateTime startFinal = startLDT.toLocalDateTime(); // convert ZDT to LDT
+            System.out.println("Start Time from DB: " + start);
+            System.out.println("Start time with conversion: " + startFinal);
 
+            //Converting end times to UTC then to LDT
             ZonedDateTime endConv = end.atZone(ZoneId.of("UTC"));
             ZonedDateTime endLDT = endConv.withZoneSameInstant(ZoneId.systemDefault());
             LocalDateTime endFinal = endLDT.toLocalDateTime();
 
+       //Create the appointment object.
        Appointments adding = new Appointments(appointmentId,title,description,location,type,startFinal,endFinal,Created,customerID,userID,contactID);
         appList.add(adding);
-            System.out.println("Appointments: " + appList);
         }
         return appList;
     }
