f26a30caa8c1e65b58fb71076f0cf561cbf615ad
Michael Evans <35019721+paradoxl@users.noreply.github.com>
2023-02-16 11:19:26 -0700

    Delete src/main/java/com/michael/c195_software2/JavaDocs directory

--- src/main/java/com/michael/c195_software2/JavaDocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.michael.c195_software2","l":"com.michael.c195_software2"},{"m":"com.michael.c195_software2","l":"com.michael.c195_software2.DataAccessObject"},{"m":"com.michael.c195_software2","l":"com.michael.c195_software2.dataBaseConnection"}];updateSearchResults();
